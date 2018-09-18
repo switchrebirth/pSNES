feca3cd40deb97efbf4a13a58879f7c38b6fecfa
Cpasjuste <cpasjuste@gmail.com>
2018-09-18 23:12:26 +0200

    remove black borders in fit/fullscreen modes

--- psnes/video.cpp
+++ psnes/video.cpp
@@ -17,61 +17,61 @@ PSNESVideo::PSNESVideo(C2DUIGuiMain *gui, void **_pixels, int *_pitch, const c2d
 
 void PSNESVideo::updateScaling() {
 
-    int scale_mode = 0;
-    const char *scale_string = ui->getConfig()->getOption(
-            ui->getConfig()->getOptions(true), C2DUIOption::Index::ROM_SCALING)->getValue();
-    if (strcmp(scale_string, "3X") == 0) {
-        scale_mode = 2;
-    } else if (strcmp(scale_string, "FIT") == 0) {
-        scale_mode = 3;
-    } else if (strcmp(scale_string, "FIT 4:3") == 0) {
-        scale_mode = 4;
-    } else if (strcmp(scale_string, "FULL") == 0) {
-        scale_mode = 5;
-    }
-
+    int high_res = ui->getConfig()->getValue(C2DUIOption::ROM_HIGH_RES, true);
+    int scale_mode = ui->getConfig()->getValue(C2DUIOption::ROM_SCALING, true);
     Vector2f screen = ui->getRenderer()->getSize();
     Vector2f scale_max;
-    float sx = 1, sy = 1;
+    float sx = 1, sy = 1, factor;
+    bool integer_scaling = false;
 
     scale_max.x = screen.x / getTextureRect().width;
     scale_max.y = screen.y / getTextureRect().height;
 
     switch (scale_mode) {
 
-        case 2: // 3x (2x software scaling already applied)
-            sx = sy = std::min(scale_max.x, 1.5f);
-            if (sy > scale_max.y) {
-                sx = sy = std::min(scale_max.y, 1.5f);
+        default:
+            // 2x in non "high res" mode (2x software scaling already applied in high res mode)
+            if (!high_res) {
+                sx = sy = std::min(scale_max.x, 2.0f);
+                if (sy > scale_max.y) {
+                    sx = sy = std::min(scale_max.y, 2.0f);
+                }
             }
+            integer_scaling = true;
             break;
 
-        case 3: // fit
+        case 1:
+            // 3x (2x software scaling already applied in high res mode)
+            factor = high_res ? 1.5f : 3.0f;
+            sx = sy = std::min(scale_max.x, factor);
+            if (sy > scale_max.y) {
+                sx = sy = std::min(scale_max.y, factor);
+            }
+            integer_scaling = true;
+            break;
+
+        case 2: // fit
             sx = sy = scale_max.y;
             if (sx > scale_max.x) {
                 sx = sy = scale_max.x;
             }
             break;
 
-        case 4: // fit 4:3
+        case 3: // fit 4:3
             sy = scale_max.y;
             sx = std::min(scale_max.x, (sy * getTextureRect().height * 1.33f) / getTextureRect().width);
             break;
 
-        case 5: // fullscreen
+        case 4: // fullscreen
             sx = scale_max.x;
             sy = scale_max.y;
-            break;
-
-        default:
             break;
     }
 
     setOriginCenter();
     // remove snes9x border if needed
     float posY = snes9x_height_extended ? screen.y / 2 : (screen.y / 2) * 1.065f;
-    // TODO: invalid shader effect when scaling ?!
-    float scaleY = sy;//snes9x_height_extended ? sy : sy * 1.065f;
+    float scaleY = snes9x_height_extended || integer_scaling ? sy : sy * 1.065f;
     setPosition(screen.x / 2, posY);
     setScale(sx, scaleY);
 }
