6c62f22a2169ccab0e5861f275a67e0c1db34367
Cpasjuste <cpasjuste@gmail.com>
2018-09-19 16:50:40 +0200

    show warning message when using HIGH_RES mode

--- psnes/config.cpp
+++ psnes/config.cpp
@@ -61,6 +61,9 @@ PSNESConfig::PSNESConfig(Renderer *renderer, const std::string &home, int versio
             C2DUIOption("SHOW_FPS", {"OFF", "ON"}, 0, C2DUIOption::Index::ROM_SHOW_FPS));
     ui_options->emplace_back(
             C2DUIOption("HIGH_RES", {"OFF", "ON"}, 0, C2DUIOption::Index::ROM_HIGH_RES));
+    ui_options->at(ui_options->size() - 1).setInfo("Only enable high resolution mode for games which can use it,\n"
+                                                   "for example \"Secret Of Mana\".\n\n"
+                                                   "It does have some performance impact.");
     ui_options->emplace_back(
             C2DUIOption("CHEATS", {"OFF", "ON"}, 1, C2DUIOption::Index::ROM_CHEATS));
 
